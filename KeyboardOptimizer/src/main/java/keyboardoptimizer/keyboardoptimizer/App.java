ac1735846a3a4dbcede2d250aca0c7d2a49bec82
MarshallRTEast <atte.hanski@helsinki.fi>
2014-03-15 12:16:11 +0200

    Toivottavasti korjaa kansiorakenteen

---
+++ KeyboardOptimizer/src/main/java/keyboardoptimizer/keyboardoptimizer/App.java
@@ -0,0 +1,13 @@
+package keyboardoptimizer.keyboardoptimizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
